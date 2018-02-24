a278098da1c03425421eb122cc76c0c79a006c1f
Elesmer <paelesmer@gmail.com>
2018-02-24 23:51:27 +0200

    Test

--- js/common.js
+++ js/common.js
@@ -1,22 +1,18 @@
 jQuery(document).ready(function() {
 
-	/* Parallax */
+	/* Tabs */
 
-	$('.parallax').parallax();
+		$('#tabs-swipe-demo').tabs();
 
-	/* Mobile nav */
-
-	$(".button-collapse").sideNav();
+	/* Mobile */
 
 	$('.button-collapse').sideNav({
-		menuWidth: 350, // Default is 300
+		menuWidth: 300, // Default is 300
 		edge: 'right', // Choose the horizontal origin
 		closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
 		draggable: true, // Choose whether you can drag to open on touch screens,
-		onOpen: function(el) {
-		}
-	}
-	);
+		onOpen: function(el) {}
+	});
 
 	/* Main carousel */
 
@@ -89,20 +85,5 @@ jQuery(document).ready(function() {
 	/* Wow */
 
 	new WOW().init();
-
-	/* Phone input mask */
-
-	$(".phone").mask("+380 999 999 999");
-
-	/* Scroll */
-
-	$('a[href*=#]').bind("click", function(e) {
-		var anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $(anchor.attr('href')).offset().top
-		}, 1000);
-		e.preventDefault();
-	});
-	return false;
 
 });
