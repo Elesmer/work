8c9b9ad10b763cab3294405846b64bd85188ce28
Elesmer <paelesmer@gmail.com>
2018-02-27 14:22:36 +0200

    Done

--- js/common.js
+++ js/common.js
@@ -1,19 +1,5 @@
 jQuery(document).ready(function() {
 
-	/* Tabs */
-
-		$('#tabs-swipe-demo').tabs();
-
-	/* Mobile */
-
-	$('.button-collapse').sideNav({
-		menuWidth: 300, // Default is 300
-		edge: 'right', // Choose the horizontal origin
-		closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-		draggable: true, // Choose whether you can drag to open on touch screens,
-		onOpen: function(el) {}
-	});
-
 	/* Main carousel */
 
 	var owl = $('.main-owl-carousel');
@@ -85,5 +71,20 @@ jQuery(document).ready(function() {
 	/* Wow */
 
 	new WOW().init();
+
+	/* Phone input mask */
+
+	$(".phone").mask("+380 999 999 999");
+
+	/* Scroll */
+
+	$('a[href*=#]').bind("click", function(e) {
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top
+		}, 1000);
+		e.preventDefault();
+	});
+	return false;
 
 });
