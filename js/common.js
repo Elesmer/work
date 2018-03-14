52879de260a3dbbf143a0d2433c355e43772cf54
Elesmer <paelesmer@gmail.com>
2018-03-14 11:17:16 +0200

    Add
    New project

--- js/common.js
+++ js/common.js
@@ -1,37 +1,19 @@
-jQuery(document).ready(function() {
+new WOW().init();
 
-			/* Select */
+function viewGraph() {
+  $(".column").css("height", "13px");
+  $("table tr").each(function(index) {
+    var ha = $(this)
+    .children("td")
+    .eq(1)
+    .text();
+    $("#col" + index)
+      .animate({ height: ha }, 1500)
+      .html("<div>" + ha + "</div>");
+  });
+}
+$(document).ready(function() {
+  viewGraph();
 
-			$('select').material_select();
-
-			/* Hide */
-
-			$("#error-close-1").click(function() { // class of the button div
-				$(".error--1").hide(); // class/id/classes which will hide
-				return false;
-			});
-
-			$("#error-close-2").click(function() { // class of the button div
-				$(".error--2").hide(); // class/id/classes which will hide
-				return false;
-			});
-
-			/* Tabs */
-
-			$('#tabs-swipe-demo').tabs();
-
-			/* Mobile */
-
-			$('.button-collapse').sideNav({
-				menuWidth: 300, // Default is 300
-				edge: 'right', // Choose the horizontal origin
-				closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-				draggable: true, // Choose whether you can drag to open on touch screens,
-				onOpen: function(el) {}
-			});
-
-			/* Wow */
-
-			new WOW().init();
-
+  $('.parallax').parallax();
 });
