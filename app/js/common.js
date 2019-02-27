3f594b4afb2645ec0831efebb95b9da373356acb
Elesmer <paelesmer@gmail.com>
2019-02-27 17:04:36 +0200

    About us section - done.

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,25 @@
-$(function() {
+jQuery(document).ready(function($) {
 
-	// Custom JS
+  // About us carousel init
 
+
+  $('.about-us__carousel').owlCarousel({
+    loop: true,
+    items: 1,
+    nav: false,
+    dots: false,
+    autoplay: true
+  });
+
+
+  // About us carousel nav
+
+  var aboutUsCarouselNav = $('.about-us__carousel');
+  aboutUsCarouselNav.owlCarousel();
+  $('.about-us__next').click(function() {
+    aboutUsCarouselNav.trigger('next.owl.carousel');
+  })
+  $('.about-us__prev').click(function() {
+    aboutUsCarouselNav.trigger('prev.owl.carousel', [300]);
+  })
 });
