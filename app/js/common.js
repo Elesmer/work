e9e003539d04c3d7f6ef9fec15f654f8dfe01c38
Andriy Pavlyshyn <paelesmer@gmail.com>
2019-02-27 13:07:05 +0200

    New project

--- app/js/common.js
+++ app/js/common.js
@@ -1,34 +1,5 @@
-jQuery(document).ready(function() {
+$(function() {
 
-	//parallax
-
-	$('.parallax').parallax();
-
-	viewGraph();
-
-	new WOW().init();
-
-	$("a.chart__a--ancor").click(function() {
-		 $("html, body").animate({
-				scrollTop: $($(this).attr("href")).offset().top + "px"
-		 }, {
-				duration: 500,
-				easing: "swing"
-		 });
-		 return false;
-	});
+	// Custom JS
 
 });
-
-function viewGraph() {
-	$(".column").css("height", "13px");
-	$("table tr").each(function(index) {
-		var ha = $(this)
-			.children("td")
-			.eq(1)
-			.text();
-		$("#col" + index)
-			.animate({ height: ha }, 1500)
-			.html("<div>" + ha + "</div>");
-	});
-};
