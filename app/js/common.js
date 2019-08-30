2ffddedfe066b5191b7962aa18d977a2d318b620
Andrii Pavlyshyn <paelesmer@gmail.com>
2019-08-30 21:33:51 +0300

    Add form section, notifications and errors.

--- app/js/common.js
+++ app/js/common.js
@@ -1,30 +1,31 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
+  jQuery("#form_send").submit(function() {
+    jQuery.ajax({
+      type: "POST",
+      url: "/wp-content/themes/form/sendEmail.php",
+      data: jQuery(this).serialize(),
+      success: function(res) {
+        jQuery('#form_send')[0].reset();
+        $("#successBlock").addClass("success-block--done");
+      },
+      error:
+        UIkit.notification({
+          message: '<i class="far fa-frown"></i><p>something wrong!</p>',
+          status: 'primary',
+          pos: 'top-right',
+          timeout: 50000
+        }),
+    }).done(function() {});
+    return false;
+  });
 });
 
-// Resize sticky header
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 250) {
-    $('header').addClass("sticky");
-  } else {
-    $('header').removeClass("sticky");
-  }
-});
-
-/* To top button */
-
-var offset = 300,
-    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-    offset_opacity = 1200,
-    //duration of the top scrolling animation (in ms)
-    scroll_top_duration = 700,
-    //grab the "back to top" link
-    $back_to_top = $('.js-back-to-top');
-
-//hide or show the "back to top" link
-$(window).scroll(function(){
-    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('back-to-top-is-visible') : $back_to_top.removeClass('back-to-top-is-visible back-to-top-fade-out');
-    if( $(this).scrollTop() > offset_opacity ) {
-        $back_to_top.addClass('back-to-top-fade-out');
-    }
-});
+// // Resize sticky header
+// $(window).scroll(function() {
+//   if ($(this).scrollTop() > 250) {
+//     $('header').addClass("sticky");
+//   } else {
+//     $('header').removeClass("sticky");
+//   }
+// });
