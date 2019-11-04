3ba197df4a0b8e25134d02f4c232504fdb02012c
Andrii Pavlyshyn <paelesmer@gmail.com>
2019-11-04 22:26:04 +0200

    Add

--- app/js/common.js
+++ app/js/common.js
@@ -2,3 +2,30 @@ $(document).ready(function () {
 
 
 });
+
+// Resize sticky header
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 250) {
+    $('header').addClass("sticky");
+  } else {
+    $('header').removeClass("sticky");
+  }
+});
+
+/* To top button */
+
+var offset = 300,
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    offset_opacity = 1200,
+    //duration of the top scrolling animation (in ms)
+    scroll_top_duration = 700,
+    //grab the "back to top" link
+    $back_to_top = $('.js-back-to-top');
+
+//hide or show the "back to top" link
+$(window).scroll(function(){
+    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('back-to-top-is-visible') : $back_to_top.removeClass('back-to-top-is-visible back-to-top-fade-out');
+    if( $(this).scrollTop() > offset_opacity ) {
+        $back_to_top.addClass('back-to-top-fade-out');
+    }
+});
--- gulpfile.js
+++ gulpfile.js
@@ -65,9 +65,9 @@ gulp.task('styles', function() {
 
 gulp.task('scripts', function() {
   return gulp.src([
-      // 'app/libs/jquery/dist/jquery.min.js',
-			// 'app/libs/uikit/uikit.min.js',
-			// 'app/libs/uikit/uikit-icons.min.js',
+      'app/libs/jquery/dist/jquery.min.js',
+			'app/libs/uikit/uikit.min.js',
+			'app/libs/uikit/uikit-icons.min.js',
       'app/js/common.js', // Always at the end
     ])
     .pipe(concat('scripts.min.js'))
