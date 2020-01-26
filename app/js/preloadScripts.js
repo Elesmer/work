1fbf770255a0957d233ef176e8fdc36a0943ad7e
Andrii Pavlyshyn <paelesmer@gmail.com>
2020-01-26 18:21:45 +0200

    Fix aniamations

--- app/js/preloadScripts.js
+++ app/js/preloadScripts.js
@@ -50,42 +50,42 @@ document.body.onload = function() {
     let secondPixel = document.getElementById('secondPixel');
     let thirdPixel = document.getElementById('thirdPixel');
 
-    // anime.remove(['.preloader .d','.preloader .c','.preloader .b']);
-    //
-    // anime({
-    //   targets: '.preloader #firstPixel',
-    //   translateX: -60,
-    //   translateY: -250,
-    //   // rotate: function() { return anime.random(-100, 200); },
-    //   duration:  1000,
-    //   direction: 'normal',
-    //   scale: 0.5,
-    //   delay: 200,
-    //   easing: 'cubicBezier(.5, .05, .1, .3)'
-    // });
-    //
-    // anime({
-    //   targets: '.preloader #secondPixel',
-    //   translateX: -60,
-    //   translateY: -250,
-    //   // rotate: function() { return anime.random(-100, 200); },
-    //   duration:  1000,
-    //   direction: 'normal',
-    //   scale: 0.5,
-    //   delay: 300,
-    //   easing: 'cubicBezier(.5, .05, .1, .3)'
-    // });
-    //
-    // anime({
-    //   targets: '.preloader #thirdPixel',
-    //   translateX: -60,
-    //   translateY: -250,
-    //   // rotate: function() { return anime.random(-100, 200); },
-    //   duration:  1000,
-    //   direction: 'normal',
-    //   scale: 0.5,
-    //   easing: 'cubicBezier(.5, .05, .1, .3)'
-    // });
+    anime.remove(['.preloader .d','.preloader .c','.preloader .b']);
+
+    anime({
+      targets: '.preloader #firstPixel',
+      translateX: -60,
+      translateY: -250,
+      // rotate: function() { return anime.random(-100, 200); },
+      duration:  1000,
+      direction: 'normal',
+      scale: 0.5,
+      delay: 200,
+      easing: 'cubicBezier(.5, .05, .1, .3)'
+    });
+
+    anime({
+      targets: '.preloader #secondPixel',
+      translateX: -60,
+      translateY: -250,
+      // rotate: function() { return anime.random(-100, 200); },
+      duration:  1000,
+      direction: 'normal',
+      scale: 0.5,
+      delay: 300,
+      easing: 'cubicBezier(.5, .05, .1, .3)'
+    });
+
+    anime({
+      targets: '.preloader #thirdPixel',
+      translateX: -60,
+      translateY: -250,
+      // rotate: function() { return anime.random(-100, 200); },
+      duration:  1000,
+      direction: 'normal',
+      scale: 0.5,
+      easing: 'cubicBezier(.5, .05, .1, .3)'
+    });
 
   }, 2000);
 
